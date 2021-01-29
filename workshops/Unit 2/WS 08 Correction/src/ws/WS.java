c4b3d1c9d3c64ef115b90bdd2738b1ab6528effc
Carlos Iván Jiménez Muñoz <75271696+CarlosJimenez435@users.noreply.github.com>
2021-01-29 16:00:49 -0500

    Add files via upload

---
+++ workshops/Unit 2/WS 08 Correction/src/ws/WS.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ws;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author USER
+ */
+public class WS {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       
+        int multiplicand = 0;
+        int multiplier = 1;
+        int product;
+        int stop = 12;
+        
+       product = 0;
+       do{
+           System.out.println("Enter the table you want learn:");
+           System.out.println("0 to exit :");
+           Scanner input = new Scanner (System.in);
+           multiplicand = input.nextInt();
+           
+           
+           System.out.println("Sstudying the table of ---->" + multiplicand + "<-----");
+           for (multiplier = 1; multiplier <= stop;multiplier++){
+               product = multiplicand * multiplier;
+               System.out.println(multiplicand + "*" + multiplier + "=" + product);
+                            
+                  }           
+           multiplier = 1;
+           
+            
+    } while (multiplicand !=0);
+           System.out.println("See you later");
+           
+           
+           
+           
+       }
+    }
+    
+
