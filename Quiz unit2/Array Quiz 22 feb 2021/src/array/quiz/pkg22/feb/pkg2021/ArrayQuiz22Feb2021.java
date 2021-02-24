a35561c84c559f9cb7eedb8921f72fd50847dd5b
Carlos Iván Jiménez Muñoz <75271696+CarlosJimenez435@users.noreply.github.com>
2021-02-24 08:47:17 -0500

    Add files via upload

---
+++ Quiz unit2/Array Quiz 22 feb 2021/src/array/quiz/pkg22/feb/pkg2021/ArrayQuiz22Feb2021.java
@@ -0,0 +1,46 @@
+
+package array.quiz.pkg22.feb.pkg2021;
+
+public class ArrayQuiz22Feb2021 {
+
+
+
+    public static void main(String[] args) {
+        
+       float[] Tools = new float[10];
+ 
+        
+       Tools[0] =  17;   
+       Tools[1] =  2;
+       Tools[2] =  5;
+       Tools[3] =  12;  
+       Tools[4] =  13;   
+       Tools[5] =  11; 
+       Tools[6] =  20;  
+       Tools[7] =  8;
+       Tools[8] =  6;
+       Tools[9] =  5;    
+       
+       for (float i=0; i<10; i++){
+           System.out.println("THE PRICE OF TOOLS IN DOLARS:");
+           
+           System.out.println("The price of hammer is: $" + Tools[0]);
+           System.out.println("The price of nails is: $" + Tools[1] );
+           System.out.println("The price of saw is: $" + Tools[2]);
+           System.out.println("The price of grinder is: $" + Tools[3]);
+           System.out.println("The price of spanner is: $" + Tools[4]);
+           System.out.println("The price of screwdriver is: $" + Tools[5]);
+           System.out.println("The price of tweezers is: $" + Tools[6]);
+           System.out.println("The price of screws is: $" + Tools[7]);
+           System.out.println("The price of measuring tape is: $" + Tools[9]);
+           System.out.println("The price of lime is: $" + Tools[9]);
+           
+           
+           
+       }
+      
+               
+               
+               }
+    
+}
