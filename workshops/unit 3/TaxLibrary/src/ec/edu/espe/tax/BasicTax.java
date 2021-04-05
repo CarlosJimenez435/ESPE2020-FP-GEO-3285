271869f43f7bac92fd0abefb7389c1c6e98982cd
Carlos Iván Jiménez Muñoz <75271696+CarlosJimenez435@users.noreply.github.com>
2021-04-05 13:04:36 -0500

    Add files via upload

---
+++ workshops/unit 3/TaxLibrary/src/ec/edu/espe/tax/BasicTax.java
@@ -0,0 +1,26 @@
+
+package ec.edu.espe.tax;
+
+public class BasicTax {
+    
+   public static float computeIva(float basePrice, float percentageValue){
+       
+       float iva;
+       iva = basePrice * percentageValue/100;
+       return iva;
+       
+       
+   } 
+
+    public static float[] computeIva(float[] priceMatrixA, float IvaPercentageMatrixD) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public static float[] computeIva(float[] priceMatrixA, float IvaPercentageMatrixD) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public static float[] computeIva(float[] priceMatrixA, float IvaPercentageMatrixD) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+}
